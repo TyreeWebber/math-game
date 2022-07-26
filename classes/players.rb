ef8778eb6ff51c271afa09e235ba3ab788e553c0
Tyree Webber <webber.tyree@gmail.com>
2022-07-25 19:57:41 -0600

    create players class

---
+++ classes/players.rb
@@ -0,0 +1,17 @@
+class Players
+
+  attr_reader :lives, :player
+  def initialize  (player)
+    @player = player
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+  def alive?
+    @lives > 0
+  end
+
+end
